ba3e7be9b33ea2074d1bd63a91882f1a959fbd56
MaxMifkovic <mmifkovi@mymail.mines.edu>
2018-08-23 12:39:15 -0600

    Added documentation to mad() in statistics.py

--- mstatistics/statistics.py
+++ mstatistics/statistics.py
@@ -12,4 +12,24 @@ def hello_world():
     return ("Hello, World!")
 
 def mad(a):
-    return np.mean(np.absolute(a - np.mean(a)))
+    """
+    Compute the mean absolute deviation of an array or matrix.
+
+    Parameters
+    ----------
+    a : array_like
+        Input array
+
+    Returns
+    -------
+    out : float
+        The mean absolute deviation.
+
+    References
+    ----------
+    https://en.wikipedia.org/wiki/Average_absolute_deviation
+
+    """
+
+    out = np.mean(np.absolute(a - np.mean(a)))
+    return out
