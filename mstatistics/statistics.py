cef132d004578a4d8e1a2a3c2ee41b100ca2bc4d
MaxMifkovic <mmifkovi@mymail.mines.edu>
2018-08-23 12:31:47 -0600

    Added 'mad' routine to statistics.py to calculated the Mean Absolute Deviation

--- mstatistics/statistics.py
+++ mstatistics/statistics.py
@@ -1,2 +1,15 @@
+"""
+Statistics
+
+Routines in this module:
+
+mad(a)
+"""
+
+import numpy as np
+
 def hello_world():
     return ("Hello, World!")
+
+def mad(a):
+    return np.mean(np.absolute(a - np.mean(a)))
