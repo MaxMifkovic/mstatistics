3b5b5ed03b256a2b7211d403377822369383143c
MaxMifkovic <mmifkovi@mymail.mines.edu>
2018-08-24 10:00:07 -0600

    Added first test to 'test_statistics.py' for testing 'mad' function.

--- mstatistics/tests/test_statistics.py
+++ mstatistics/tests/test_statistics.py
@@ -1,7 +1,8 @@
 import pytest
+import numpy as np
 
 from ..statistics import *
 
-@requires('numpy')
 def test_mad():
     assert mad(np.arange(1, 6)) - 6/5 <= 1e-5
+    assert mad(np.arange(1, 6)) == 6/5
